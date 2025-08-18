b731faf33f1001505d0731e51d29ab0922db52e5
LuizOtaviooo <00001090614068sp@al.educacao.sp.gov.br>
2025-08-18 08:33:53 -0300

    ,,,

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded') function(){
+    // const aumentaFonteBotao = document.getElementById(`aumentar-fonte`);
+ let tamanhoAtualFonte =1;
+    aumentaFonteBotao.addEventListener('clic,' function){
+     tamanhoAtualFonte += 0.1;
+     document.body.style.fontSize = '${tamanhoAtualFonte}rem'
+
+    
+    
+    
+    })
+
+
+
+}//falta parenteses?
